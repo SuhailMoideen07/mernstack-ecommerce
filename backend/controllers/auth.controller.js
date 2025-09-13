8a0f3b522ccae5f1fcb2184f9755e8f408c40636
Suhail Moideen <suhailmoideen7@gmail.com>
2025-09-13 12:28:08 +0530

    feat: added user authentication routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,33 @@
+import User from "../models/user.model.js"
+export const signup = async (req, res) => {
+    const {email, password, name} = req.body;
+   try {
+    const userExists = await User.findOne({ email });
+    if(userExists){
+        return res.status(400).json({message: "This email already exists"});
+    }
+    const user = await User.create({name,email,password})
+//authenticate
+    const {accessToken, refreshToken} = 
+    res.status(201).json({user,message:"User created successfully"})
+    
+   } catch (error) {
+    res.status(500).json({message: error.message})
+    
+   }
+    const userExists = await User.findOne({ email });
+    if(userExists){
+        return res.status(400).json({message: "This email already exists"});
+    }
+    const user = await User.create({name,email,password})
+    res.status(201).json({user,message:"User created successfully"})
+    
+}
+
+export const login = async (req, res) => {
+    res.send("Login route called");
+}
+
+export const logout = async (req,res)=>{
+    res.send("Logout function called")
+}
