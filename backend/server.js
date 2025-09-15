500e8ca212d399e625946095fca73768e2967602
Suhail Moideen <suhailmoideen7@gmail.com>
2025-09-15 20:59:56 +0530

    user signup login and logout routes completed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authroutes from "./routes/auth.routes.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //allows to parse body of request
+app.use(cookieParser());
 app.use("/api/auth", authroutes)
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
