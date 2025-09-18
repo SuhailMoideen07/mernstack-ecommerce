75d996a0d31ecfa5d039e5f6173963f66953f4aa
Suhail Moideen <suhailmoideen7@gmail.com>
2025-09-19 00:28:51 +0530

    middlewares updated

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,5 +1,7 @@
 import express from "express"
 import { getAllProducts } from "../controllers/product.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { adminRoute } from "../middleware/auth.middleware.js";
  const router = express.Router();
  
 
