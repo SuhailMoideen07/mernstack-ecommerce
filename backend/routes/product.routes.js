3b82a808c3e86e04e6e999899bef62365929ed4c
Suhail Moideen <suhailmoideen7@gmail.com>
2025-09-18 22:46:03 +0530

    Product routes updated

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,1 +1,7 @@
 import express from "express"
+import { getAllProducts } from "../controllers/product.controller.js";
+ const router = express.Router();
+ 
+
+ router.get("/", protectRoute,adminRoute, getAllProducts)
+ export default router
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import authroutes from "./routes/auth.routes.js";
-import productroutes from "./routes/product.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import productRoutes from "./routes/product.routes.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -12,8 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //allows to parse body of request
 app.use(cookieParser());
-app.use("/api/auth", authroutes)
-app.use("/api/product",productroutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/product",productRoutes)
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
     connectDB();
