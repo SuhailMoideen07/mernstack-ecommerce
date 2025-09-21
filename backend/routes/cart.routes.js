31066e5994eeef757c1e721c2fdcc2ae05715912
Suhail Moideen <suhailmoideen7@gmail.com>
2025-09-21 20:38:08 +0530

    Cart routes updated

--- backend/routes/cart.routes.js
+++ backend/routes/cart.routes.js
@@ -1,10 +1,15 @@
 import express from "express"
-import addToCart from "../controllers/cart.controller.js"
+import { updateQuantity, removeAllFromCart,addToCart,getCartProducts } from "../controllers/cart.controller";
 import { protectRoute } from "../middleware/auth.middleware";
+
 
 const router = express.Router();
 
+router.get("/",protectRoute,getCartProducts)
 router.post("/",protectRoute,addToCart)
+router.delete("/",protectRoute,removeAllFromCart)
+router.delete("/:id",protectRoute,updateQuantity)
+
 
 
 export default router 
