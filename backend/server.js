a9ee5abfc2e9e8c74680884ff9515aa57c36ba50
Suhail Moideen <suhailmoideen7@gmail.com>
2025-09-22 23:20:01 +0530

    coupen routes added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js"
+import coupenRoutes from "./routes/coupen.routes.js"
 import cartRoutes from "./routes/cart.routes.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -16,6 +17,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/products",productRoutes)
 app.use("api/cart",cartRoutes)
+app.use("api/coupen",coupenRoutes)
+
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
     connectDB();
